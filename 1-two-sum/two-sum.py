23a4f2009711c6a63f5170353421450899f4a8fd
anyoneabd2044-art <anyoneabd2044@gmail.com>
2026-01-27 18:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (32.61%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in num_to_index:
+                return[num_to_index[complement], i]
+            num_to_index[nums[i]] = i
+        
+        
